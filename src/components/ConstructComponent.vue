82ce4ff2bce2872975522d087c22d6f78f7ee025
Lucas Dantas Nogueira <lucas.dantas@creditcash.com.br>
2023-06-09 12:08:01 -0300

    Configurações realzadas e imagem de construção adicionada

---
+++ src/components/ConstructComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full h-full flex justify-center items-center img">
+        <!-- <div class="flex flex-col items-start">
+            <h1 class="text-3xl text-indigo-800 font-bold">Em Construção!</h1>
+            <p class="text-2xl text-indigo-800">Nosso site está em construção, mas estamos prontos para começar! Omlyy vem aí ;) </p>
+        </div>         -->
+    </div>
+</template>
+
+<style scoped>
+.img {
+    background-image: url('../assets/construcao.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
