186e59fc6ea9448d2b8fd1815631b09ceb0cd170
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-06-13 10:38:12 -0300

    Ajuste tela de connstrução

--- src/components/ConstructComponent.vue
+++ src/components/ConstructComponent.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="w-full h-full flex justify-center items-center img">
-        <!-- <div class="flex flex-col items-start">
-            <h1 class="text-3xl text-indigo-800 font-bold">Em Construção!</h1>
-            <p class="text-2xl text-indigo-800">Nosso site está em construção, mas estamos prontos para começar! Omlyy vem aí ;) </p>
-        </div>         -->
+    <div class="w-full h-full flex flex-col-reverse justify-center items-center sm:flex-row">
+        <div class="flex flex-col items-center text-center mb-6">
+            <h1 class="text-6xl text-indigo-800 font-bold">Ooops!</h1>
+
+            <h3 class="text-xl text-indigo-800 font-semibold m-3 mt-1">Site em Construção</h3>
+
+            <p class="text-xl text-indigo-800 font-semibold mb-3">Em breve estaremos com novidades ;)</p>
+
+            <button 
+                class="bg-indigo-600 text-white border py-2 px-5 rounded-md font-semibold hover:bg-indigo-500"
+            > 
+                Mais Informação
+            </button>
+        </div>
+        <div class="flex">
+            <img src="../assets/construcao.jpg" alt="">
+        </div>
     </div>
 </template>
 
 <style scoped>
-.img {
-    background-image: url('../assets/construcao.png');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
 </style>
